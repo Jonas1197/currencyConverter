7c97630bea497354dba4e7c31c716e3215f1e0ca
Jonas G <jonasgamburg97@gmail.com>
2022-07-29 12:43:11 +0300

    Settings - Donation button
    - Added a donation button.

--- CurrencyConverter/ViewControllers/SettingsViewController/SettingsViewController.swift
+++ CurrencyConverter/ViewControllers/SettingsViewController/SettingsViewController.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class SettingsViewController: BaseViewController<SettingsViewModel> {
-
+final class SettingsViewController: BaseViewController<SettingsViewModel> {
     
     @IBOutlet weak var radiusValueLabel: UILabel!
     @IBOutlet weak var radiusSlider:     UISlider!
-    
+    @IBOutlet weak var donateButton: UIButton!
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
@@ -21,27 +20,36 @@ class SettingsViewController: BaseViewController<SettingsViewModel> {
         UIDevice.forceOrientation(.portrait)
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        donateButton.rounded(donateButton.frame.height / 4)
+    }
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         viewModel.output?.settingsDidDisappear()
     }
 
-
     //MARK: - SetUp
     private func setUp() {
         radiusValueLabel.text = "\(UserManager.shared.zoomRadius) meters"
+        donateButton.shadowed(with: .black, offset: .init(width: 0, height: 2), radius: 12, 0.3)
     }
     
     //MARK: - Actions
     @IBAction func radiusValueChanged(_ sender: UISlider) {
-        print("\n~~> Zoom radius value: \(sender.value)")
-        
-        let value = Int(sender.value)
-        radiusValueLabel.text = "\(value) meters"
-        
+        let value                     = Int(sender.value)
+        radiusValueLabel.text         = "\(value) meters"
         UserManager.shared.zoomRadius = value
         
+        // If you want to update the map view live.
 //        viewModel.output?.settingsUpdated()
+    }
+    
+    @IBAction func donateButtonTapped(_ sender: UIButton) {
+        sender.actionWithSpringAnimation {
+            //
+        }
     }
     
 }
